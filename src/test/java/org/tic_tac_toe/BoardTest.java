0c6ccb341878a50a35b59fed2849736b92e274fd
alvi103-png <alvi103@gmail.com>
2026-06-11 10:28:10 +0200

    add: folders and files for test, BoardTest.java and PlayerTest.java

---
+++ src/test/java/org/tic_tac_toe/BoardTest.java
@@ -0,0 +1,8 @@
+package org.tic_tac_toe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+}
---
+++ src/test/java/org/tic_tac_toe/PlayerTest.java
@@ -0,0 +1,8 @@
+package org.tic_tac_toe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+}
